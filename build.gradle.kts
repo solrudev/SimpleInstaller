eb625b9f6316e5f16d289b7dd0155fededf4228e
Ilya Fomichev <fomil.music@gmail.com>
2021-11-30 04:28:20 +0700

    set up maven publishing + github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+	id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
 buildscript {
 	repositories {
 		google()
@@ -11,4 +18,43 @@ buildscript {
 
 tasks.register<Delete>("clean").configure {
 	delete(rootProject.buildDir)
+}
+
+val ossrhUsername by extra("")
+val ossrhPassword by extra("")
+val sonatypeStagingProfileId by extra("")
+extra["signing.keyId"] = ""
+extra["signing.password"] = ""
+extra["signing.key"] = ""
+
+val secretPropertiesFile = project.rootProject.file("local.properties")
+if (secretPropertiesFile.exists()) {
+	with(Properties()) {
+		FileInputStream(secretPropertiesFile).use { load(it) }
+		forEach { name, value -> extra[name as String] = value }
+	}
+}
+extra["ossrhUsername"] =
+	System.getenv("OSSRH_USERNAME") ?: extra["ossrhUsername"]
+extra["ossrhPassword"] =
+	System.getenv("OSSRH_PASSWORD") ?: extra["ossrhPassword"]
+extra["sonatypeStagingProfileId"] =
+	System.getenv("SONATYPE_STAGING_PROFILE_ID") ?: extra["sonatypeStagingProfileId"]
+extra["signing.keyId"] =
+	System.getenv("SIGNING_KEY_ID") ?: extra["signing.keyId"]
+extra["signing.password"] =
+	System.getenv("SIGNING_PASSWORD") ?: extra["signing.password"]
+extra["signing.key"] =
+	System.getenv("SIGNING_KEY") ?: extra["signing.key"]
+
+nexusPublishing {
+	repositories {
+		sonatype {
+			stagingProfileId.set(sonatypeStagingProfileId)
+			username.set(ossrhUsername)
+			password.set(ossrhPassword)
+			nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+			snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+		}
+	}
 }
--- simpleinstaller/build.gradle.kts
+++ simpleinstaller/build.gradle.kts
@@ -1,6 +1,12 @@
+val publishGroupId = "io.github.solrudev"
+val publishArtifactId = "simpleinstaller"
+val publishVersion = "1.0.0"
+
 plugins {
 	id("com.android.library")
 	kotlin("android")
+	`maven-publish`
+	signing
 }
 
 android {
@@ -9,8 +15,12 @@ android {
 	defaultConfig {
 		minSdk = 16
 		targetSdk = 31
-		version = "1.0.0"
+		version = publishVersion
 		consumerProguardFiles("consumer-rules.pro")
+	}
+
+	sourceSets {
+		named("main").get().java.srcDirs("src/main/kotlin")
 	}
 
 	buildTypes {
@@ -24,10 +34,12 @@ android {
 			)
 		}
 	}
+
 	compileOptions {
 		sourceCompatibility = JavaVersion.VERSION_1_8
 		targetCompatibility = JavaVersion.VERSION_1_8
 	}
+
 	kotlinOptions {
 		jvmTarget = "1.8"
 	}
@@ -42,4 +54,63 @@ dependencies {
 	api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
 	api("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 	implementation("com.squareup.okio:okio:2.10.0")
+}
+
+tasks {
+	val androidSourcesJar by creating(Jar::class) {
+		archiveClassifier.set("sources")
+		from(android.sourceSets.getByName("main").java.srcDirs)
+	}
+
+	artifacts {
+		archives(androidSourcesJar)
+	}
+
+	afterEvaluate {
+		publishing {
+			publications {
+				create("release", MavenPublication::class) {
+					groupId = publishGroupId
+					artifactId = publishArtifactId
+					version = publishVersion
+					from(components.getByName("release"))
+					artifact(androidSourcesJar)
+
+					pom {
+						name.set(publishArtifactId)
+						description.set("Android packages un/install library")
+						url.set("https://github.com/solrudev/SimpleInstaller")
+
+						licenses {
+							license {
+								name.set("SimpleInstaller License")
+								url.set("https://github.com/solrudev/SimpleInstaller/blob/master/LICENSE")
+							}
+						}
+
+						developers {
+							developer {
+								id.set("solrudev")
+								name.set("Ilya Fomichev")
+							}
+						}
+
+						scm {
+							connection.set("scm:git:github.com/solrudev/SimpleInstaller.git")
+							developerConnection.set("scm:git:ssh://github.com/solrudev/SimpleInstaller.git")
+							url.set("https://github.com/solrudev/SimpleInstaller/tree/master")
+						}
+					}
+				}
+			}
+		}
+	}
+
+	signing {
+		val keyId = rootProject.extra["signing.keyId"] as String
+		val key = rootProject.extra["signing.key"] as String
+		val password = rootProject.extra["signing.password"] as String
+		useInMemoryPgpKeys(keyId, key, password)
+		sign(publishing.publications)
+	}
 }
